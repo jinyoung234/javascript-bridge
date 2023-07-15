1c967be2cd92a238eb45e2a1ab367ab283904270
jinyoung <sm099@naver.com>
2023-07-15 22:42:36 +0900

    test(create-phase): 다리 길이 입력 관련 예외 처리 관련 테스트 코드 추가

--- __tests__/AdditionalTest.js
+++ __tests__/AdditionalTest.js
@@ -8,11 +8,45 @@ const getLogSpy = () => {
   return logSpy;
 };
 
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce(
+    (acc, input) =>
+      acc.mockImplementationOnce((_, callback) => {
+        callback(input);
+      }),
+    MissionUtils.Console.readLine,
+  );
+};
+const getOutput = (logSpy) => [...logSpy.mock.calls];
+
+const expectLogContains = (received, logs) => {
+  logs.forEach((log) => {
+    expect(received).toEqual(expect.stringContaining(log));
+  });
+};
+
+const runException = (inputs) => {
+  mockQuestions(inputs);
+  const app = new App();
+  app.play();
+  getOutput(getLogSpy()).forEach((log, i) => {
+    if (i === 0) return;
+    expectLogContains(log, '[ERROR]');
+  });
+};
+
 describe('게임 안내 기능', () => {
   test('게임이 시작되면 “다리 건너기 게임을 시작합니다.”를 출력할 수 있어야 한다.', () => {
     const log = getLogSpy();
     const app = new App();
     app.play();
     expect(log).toHaveBeenCalledWith(expect.stringContaining(GAME.START));
+  });
+});
+
+describe('초기 다리 생성 기능', () => {
+  test('사용자 입력 값에 대해 예외를 처리 할 수 있어야 한다.', () => {
+    runException(['a', '0', ' ']);
   });
 });
