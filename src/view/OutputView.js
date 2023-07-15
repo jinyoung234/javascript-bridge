1574bd894de93e3ae2e014954ae372021768ffdc
jinyoung <sm099@naver.com>
2023-07-15 19:03:04 +0900

    style : InputView.js, OutputView.js 들을 view 폴더에 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -15,6 +17,14 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult() {},
+
+  /**
+   * 콘솔로 필요한 메시지를 출력하기 위한 메서드
+   * @param {string} message - 콘솔로 출력할 메시지
+   */
+  print(message) {
+    Console.print(message);
+  },
 };
 
 module.exports = OutputView;
